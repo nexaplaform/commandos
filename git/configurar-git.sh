abe93414803bfd83ee3ee9a4e934a0c4868b38e3
nexaplaform <nexaplaform@gmail.com>
2025-04-18 17:41:28 +0200

    Capturando usuario y correo para git de forma dinamica

--- git/configurar-git.sh
+++ git/configurar-git.sh
@@ -30,20 +30,24 @@ echo
 
 # Instalar git
 if command -v git &> /dev/null; then
+
     echo "✔️  ->>>>> Ya hay un version de git instalada en esta maquina."
     git --version
     echo
 
 else 
+
     echo "🌀 ->>>>> Iniciando instalación."
     sudo apt install git -y
     
     echo "🟢 ->>>>> Verificando version de git."
     git --version
+
     echo
     echo "⚙️ ->>>>> Configurando usuario y correo"
     read -p "📝 Ingrese el nombre o el usuario para git: " git_user
     read -p "📝 Ingrese la dirección de correo para git " git_email
+
     git config --global user.name "$git_user";
     git config --global user.email "$git_email";
 fi
