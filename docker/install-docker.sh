6d60644aca407908bac2d7d57a32792f8e6448af
nexaplaform <nexaplaform@gmail.com>
2025-04-18 18:06:22 +0200

    Instalacion de docker

--- docker/install-docker.sh
+++ docker/install-docker.sh
@@ -9,12 +9,18 @@ status=$0
 ./update-message.sh $status "actualizando repositorios"
 
 sudo apt-get install ca-certificates curl
+status=$?
 
 sudo install -m 0755 -d /etc/apt/keyrings
+status=$?
+
 
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+status=$?
 
 sudo chmod a+r /etc/apt/keyrings/docker.asc
+status=$?
+
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
@@ -24,5 +30,6 @@ sudo apt-get update
 
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+status=$?
 
 sudo docker run hello-world
